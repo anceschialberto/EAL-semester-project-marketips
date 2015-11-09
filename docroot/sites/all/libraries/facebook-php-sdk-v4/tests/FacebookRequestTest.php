17659b780558b25ecb8b5ee87180f87050f27576
Alberto Anceschi <anceschialberto@icloud.com>
2015-11-09 01:38:57 +0100

    facebook php sdk

--- docroot/sites/all/libraries/facebook-php-sdk-v4/tests/FacebookRequestTest.php
+++ docroot/sites/all/libraries/facebook-php-sdk-v4/tests/FacebookRequestTest.php
@@ -1,207 +1,140 @@
 <?php
-/**
- * Copyright 2014 Facebook, Inc.
- *
- * You are hereby granted a non-exclusive, worldwide, royalty-free license to
- * use, copy, modify, and distribute this software in source code or binary
- * form for use in connection with the web services and APIs provided by
- * Facebook.
- *
- * As with any software that integrates with the Facebook platform, your use
- * of this software is subject to the Facebook Developer Principles and
- * Policies [http://developers.facebook.com/policy/]. This copyright notice
- * shall be included in all copies or substantial portions of the software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
-namespace Facebook\Tests;
 
-use Facebook\Facebook;
-use Facebook\FacebookApp;
 use Facebook\FacebookRequest;
-use Facebook\FileUpload\FacebookFile;
-use Facebook\FileUpload\FacebookVideo;
+use Facebook\FacebookSession;
 
-class FacebookRequestTest extends \PHPUnit_Framework_TestCase
+class FacebookRequestTest extends PHPUnit_Framework_TestCase
 {
-    public function testAnEmptyRequestEntityCanInstantiate()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app);
 
-        $this->assertInstanceOf('Facebook\FacebookRequest', $request);
-    }
+  public function testGetsTheLoggedInUsersProfile()
+  {
+    $response = (
+      new FacebookRequest(
+        FacebookTestHelper::$testSession,
+        'GET',
+        '/me'
+      ))->execute()->getGraphObject();
+    $this->assertNotNull($response->getProperty('id'));
+    $this->assertNotNull($response->getProperty('name'));
+  }
 
-    /**
-     * @expectedException \Facebook\Exceptions\FacebookSDKException
-     */
-    public function testAMissingAccessTokenWillThrow()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app);
+  public function testCanPostAndDelete()
+  {
+    // Create a test user
+    $params = array(
+      'name' => 'Foo User',
+    );
+    $response = (
+      new FacebookRequest(
+        new FacebookSession(FacebookTestHelper::getAppToken()),
+        'POST',
+        '/' . FacebookTestCredentials::$appId . '/accounts/test-users',
+        $params
+      ))->execute()->getGraphObject();
+    $user_id = $response->getProperty('id');
+    $this->assertNotNull($user_id);
 
-        $request->validateAccessToken();
-    }
+    // Delete test user
+    $response = (
+    new FacebookRequest(
+      new FacebookSession(FacebookTestHelper::getAppToken()),
+      'DELETE',
+      '/' . $user_id
+    ))->execute()->getGraphObject()->asArray();
+    $this->assertEquals(['success' => true], $response);
+  }
 
-    /**
-     * @expectedException \Facebook\Exceptions\FacebookSDKException
-     */
-    public function testAMissingMethodWillThrow()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app);
+  public function testETagHit()
+  {
+    $response = (
+    new FacebookRequest(
+      FacebookTestHelper::$testSession,
+      'GET',
+      '/104048449631599'
+    ))->execute();
 
-        $request->validateMethod();
-    }
+    $response = (
+    new FacebookRequest(
+      FacebookTestHelper::$testSession,
+      'GET',
+      '/104048449631599',
+      null,
+      null,
+      $response->getETag()
+    ))->execute();
 
-    /**
-     * @expectedException \Facebook\Exceptions\FacebookSDKException
-     */
-    public function testAnInvalidMethodWillThrow()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app, 'foo_token', 'FOO');
+    $this->assertTrue($response->isETagHit());
+    $this->assertNull($response->getETag());
+  }
 
-        $request->validateMethod();
-    }
+  public function testETagMiss()
+  {
+    $response = (
+    new FacebookRequest(
+      FacebookTestHelper::$testSession,
+      'GET',
+      '/104048449631599',
+      null,
+      null,
+      'someRandomValue'
+    ))->execute();
 
-    public function testGetHeadersWillAutoAppendETag()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app, null, 'GET', '/foo', [], 'fooETag');
+    $this->assertFalse($response->isETagHit());
+    $this->assertNotNull($response->getETag());
+  }
 
-        $headers = $request->getHeaders();
+  public function testGracefullyHandlesUrlAppending()
+  {
+    $params = array();
+    $url = 'https://www.foo.com/';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/', $processed_url);
 
-        $expectedHeaders = FacebookRequest::getDefaultHeaders();
-        $expectedHeaders['If-None-Match'] = 'fooETag';
+    $params = array(
+      'access_token' => 'foo',
+    );
+    $url = 'https://www.foo.com/';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/?access_token=foo', $processed_url);
 
-        $this->assertEquals($expectedHeaders, $headers);
-    }
+    $params = array(
+      'access_token' => 'foo',
+      'bar' => 'baz',
+    );
+    $url = 'https://www.foo.com/?foo=bar';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/?access_token=foo&bar=baz&foo=bar', $processed_url);
 
-    public function testGetParamsWillAutoAppendAccessTokenAndAppSecretProof()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app, 'foo_token', 'POST', '/foo', ['foo' => 'bar']);
+    $params = array(
+      'access_token' => 'foo',
+    );
+    $url = 'https://www.foo.com/?foo=bar&access_token=bar';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/?access_token=bar&foo=bar', $processed_url);
+  }
 
-        $params = $request->getParams();
+  public function testAppSecretProof()
+  {
+    $enableAppSecretProof = FacebookSession::useAppSecretProof();
 
-        $this->assertEquals([
-            'foo' => 'bar',
-            'access_token' => 'foo_token',
-            'appsecret_proof' => 'df4256903ba4e23636cc142117aa632133d75c642bd2a68955be1443bd14deb9',
-        ], $params);
-    }
+    FacebookSession::enableAppSecretProof(true);
+    $request = new FacebookRequest(
+      FacebookTestHelper::$testSession,
+      'GET',
+      '/me'
+    );
+    $this->assertTrue(isset($request->getParameters()['appsecret_proof']));
 
-    public function testAnAccessTokenCanBeSetFromTheParams()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app, null, 'POST', '/me', ['access_token' => 'bar_token']);
 
-        $accessToken = $request->getAccessToken();
+    FacebookSession::enableAppSecretProof(false);
+    $request = new FacebookRequest(
+      FacebookTestHelper::$testSession,
+      'GET',
+      '/me'
+    );
+    $this->assertFalse(isset($request->getParameters()['appsecret_proof']));
 
-        $this->assertEquals('bar_token', $accessToken);
-    }
+    FacebookSession::enableAppSecretProof($enableAppSecretProof);
+  }
 
-    /**
-     * @expectedException \Facebook\Exceptions\FacebookSDKException
-     */
-    public function testAccessTokenConflictsWillThrow()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        new FacebookRequest($app, 'foo_token', 'POST', '/me', ['access_token' => 'bar_token']);
-    }
-
-    public function testAProperUrlWillBeGenerated()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-        $getRequest = new FacebookRequest($app, 'foo_token', 'GET', '/foo', ['foo' => 'bar']);
-
-        $getUrl = $getRequest->getUrl();
-        $expectedParams = 'foo=bar&access_token=foo_token&appsecret_proof=df4256903ba4e23636cc142117aa632133d75c642bd2a68955be1443bd14deb9';
-        $expectedUrl = '/' . Facebook::DEFAULT_GRAPH_VERSION . '/foo?' . $expectedParams;
-
-        $this->assertEquals($expectedUrl, $getUrl);
-
-        $postRequest = new FacebookRequest($app, 'foo_token', 'POST', '/bar', ['foo' => 'bar']);
-
-        $postUrl = $postRequest->getUrl();
-        $expectedUrl = '/' . Facebook::DEFAULT_GRAPH_VERSION . '/bar';
-
-        $this->assertEquals($expectedUrl, $postUrl);
-    }
-
-    public function testAuthenticationParamsAreStrippedAndReapplied()
-    {
-        $app = new FacebookApp('123', 'foo_secret');
-
-        $request = new FacebookRequest(
-            $app,
-            $accessToken = 'foo_token',
-            $method = 'GET',
-            $endpoint = '/foo',
-            $params = [
-                'access_token' => 'foo_token',
-                'appsecret_proof' => 'bar_app_secret',
-                'bar' => 'baz',
-            ]
-        );
-
-        $url = $request->getUrl();
-
-        $expectedParams = 'bar=baz&access_token=foo_token&appsecret_proof=df4256903ba4e23636cc142117aa632133d75c642bd2a68955be1443bd14deb9';
-        $expectedUrl = '/' . Facebook::DEFAULT_GRAPH_VERSION . '/foo?' . $expectedParams;
-        $this->assertEquals($expectedUrl, $url);
-
-        $params = $request->getParams();
-
-        $expectedParams = [
-            'access_token' => 'foo_token',
-            'appsecret_proof' => 'df4256903ba4e23636cc142117aa632133d75c642bd2a68955be1443bd14deb9',
-            'bar' => 'baz',
-        ];
-        $this->assertEquals($expectedParams, $params);
-    }
-
-    public function testAFileCanBeAddedToParams()
-    {
-        $myFile = new FacebookFile(__DIR__ . '/foo.txt');
-        $params = [
-            'name' => 'Foo Bar',
-            'source' => $myFile,
-        ];
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app, 'foo_token', 'POST', '/foo/photos', $params);
-
-        $actualParams = $request->getParams();
-
-        $this->assertTrue($request->containsFileUploads());
-        $this->assertFalse($request->containsVideoUploads());
-        $this->assertTrue(!isset($actualParams['source']));
-        $this->assertEquals('Foo Bar', $actualParams['name']);
-    }
-
-    public function testAVideoCanBeAddedToParams()
-    {
-        $myFile = new FacebookVideo(__DIR__ . '/foo.txt');
-        $params = [
-            'name' => 'Foo Bar',
-            'source' => $myFile,
-        ];
-        $app = new FacebookApp('123', 'foo_secret');
-        $request = new FacebookRequest($app, 'foo_token', 'POST', '/foo/videos', $params);
-
-        $actualParams = $request->getParams();
-
-        $this->assertTrue($request->containsFileUploads());
-        $this->assertTrue($request->containsVideoUploads());
-        $this->assertTrue(!isset($actualParams['source']));
-        $this->assertEquals('Foo Bar', $actualParams['name']);
-    }
 }
--- docroot/sites/all/libraries/facebook-php-sdk-v4/tests/HttpClients/FacebookStreamHttpClientTest.php
+++ docroot/sites/all/libraries/facebook-php-sdk-v4/tests/HttpClients/FacebookStreamHttpClientTest.php
@@ -1,134 +1,123 @@
 <?php
-/**
- * Copyright 2014 Facebook, Inc.
- *
- * You are hereby granted a non-exclusive, worldwide, royalty-free license to
- * use, copy, modify, and distribute this software in source code or binary
- * form for use in connection with the web services and APIs provided by
- * Facebook.
- *
- * As with any software that integrates with the Facebook platform, your use
- * of this software is subject to the Facebook Developer Principles and
- * Policies [http://developers.facebook.com/policy/]. This copyright notice
- * shall be included in all copies or substantial portions of the software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
-namespace Facebook\Tests\HttpClients;
+
+require_once __DIR__ . '/AbstractTestHttpClient.php';
 
 use Mockery as m;
 use Facebook\HttpClients\FacebookStreamHttpClient;
 
 class FacebookStreamHttpClientTest extends AbstractTestHttpClient
 {
-    /**
-     * @var \Facebook\HttpClients\FacebookStream
-     */
-    protected $streamMock;
 
-    /**
-     * @var FacebookStreamHttpClient
-     */
-    protected $streamClient;
+  protected $streamMock;
+  protected $streamClient;
 
-    public function setUp()
-    {
-        $this->streamMock = m::mock('Facebook\HttpClients\FacebookStream');
-        $this->streamClient = new FacebookStreamHttpClient($this->streamMock);
-    }
+  public function setUp()
+  {
+    $this->streamMock = m::mock('Facebook\HttpClients\FacebookStream');
+    $this->streamClient = new FacebookStreamHttpClient($this->streamMock);
+  }
 
-    public function testCanCompileHeader()
-    {
-        $headers = [
-            'X-foo' => 'bar',
-            'X-bar' => 'faz',
-        ];
-        $header = $this->streamClient->compileHeader($headers);
-        $this->assertEquals("X-foo: bar\r\nX-bar: faz", $header);
-    }
+  public function tearDown()
+  {
+    m::close();
+    (new FacebookStreamHttpClient()); // Resets the static dependency injection
+  }
 
-    public function testCanSendNormalRequest()
-    {
-        $this->streamMock
-            ->shouldReceive('streamContextCreate')
-            ->once()
-            ->with(m::on(function ($arg) {
-                if (!isset($arg['http']) || !isset($arg['ssl'])) {
-                    return false;
-                }
+  public function testCanCompileHeader()
+  {
+    $this->streamClient->addRequestHeader('X-foo', 'bar');
+    $this->streamClient->addRequestHeader('X-bar', 'faz');
+    $header = $this->streamClient->compileHeader();
+    $this->assertEquals("X-foo: bar\r\nX-bar: faz", $header);
+  }
 
-                if ($arg['http'] !== [
-                        'method' => 'GET',
-                        'header' => 'X-foo: bar',
-                        'content' => 'foo_body',
-                        'timeout' => 123,
-                        'ignore_errors' => true,
-                    ]
-                ) {
-                    return false;
-                }
+  public function testCanFormatHeadersToArray()
+  {
+    $raw_header_array = explode("\n", trim($this->fakeRawHeader));
+    $header_array = FacebookStreamHttpClient::formatHeadersToArray($raw_header_array);
+    $this->assertEquals($this->fakeHeadersAsArray, $header_array);
+  }
 
-                $caInfo = array_diff_assoc($arg['ssl'], [
-                    'verify_peer' => true,
-                    'verify_peer_name' => true,
-                    'allow_self_signed' => true,
-                ]);
+  public function testCanGetHttpStatusCodeFromResponseHeader()
+  {
+    $http_code = FacebookStreamHttpClient::getStatusCodeFromHeader('HTTP/1.1 123 Foo Response');
+    $this->assertEquals('123', $http_code);
+  }
 
-                if (count($caInfo) !== 1) {
-                    return false;
-                }
+  public function testCanSendNormalRequest()
+  {
+    $this->streamMock
+      ->shouldReceive('streamContextCreate')
+      ->once()
+      ->with(m::on(function($arg) {
+            if ( ! isset($arg['http']) || ! isset($arg['ssl'])) {
+              return false;
+            }
 
-                if (1 !== preg_match('/.+\/certs\/DigiCertHighAssuranceEVRootCA\.pem$/', $caInfo['cafile'])) {
-                    return false;
-                }
+            if ($arg['http'] !== [
+                'method' => 'GET',
+                'timeout' => 60,
+                'ignore_errors' => true,
+                'header' => 'X-foo: bar',
+              ]) {
+              return false;
+            }
 
-                return true;
-            }))
-            ->andReturn(null);
-        $this->streamMock
-            ->shouldReceive('getResponseHeaders')
-            ->once()
-            ->andReturn(explode("\n", trim($this->fakeRawHeader)));
-        $this->streamMock
-            ->shouldReceive('fileGetContents')
-            ->once()
-            ->with('http://foo.com/')
-            ->andReturn($this->fakeRawBody);
+            $caInfo = array_diff_assoc($arg['ssl'], [
+                'verify_peer' => true,
+                'verify_peer_name' => true,
+                'allow_self_signed' => true,
+              ]);
 
-        $response = $this->streamClient->send('http://foo.com/', 'GET', 'foo_body', ['X-foo' => 'bar'], 123);
+            if (count($caInfo) !== 1) {
+              return false;
+            }
 
-        $this->assertInstanceOf('Facebook\Http\GraphRawResponse', $response);
-        $this->assertEquals($this->fakeRawBody, $response->getBody());
-        $this->assertEquals($this->fakeHeadersAsArray, $response->getHeaders());
-        $this->assertEquals(200, $response->getHttpResponseCode());
-    }
+            if (1 !== preg_match('/.+\/certs\/DigiCertHighAssuranceEVRootCA\.pem$/', $caInfo['cafile'])) {
+              return false;
+            }
 
-    /**
-     * @expectedException \Facebook\Exceptions\FacebookSDKException
-     */
-    public function testThrowsExceptionOnClientError()
-    {
-        $this->streamMock
-            ->shouldReceive('streamContextCreate')
-            ->once()
-            ->andReturn(null);
-        $this->streamMock
-            ->shouldReceive('getResponseHeaders')
-            ->once()
-            ->andReturn(null);
-        $this->streamMock
-            ->shouldReceive('fileGetContents')
-            ->once()
-            ->with('http://foo.com/')
-            ->andReturn(false);
+            return true;
+          }))
+      ->andReturn(null);
+    $this->streamMock
+      ->shouldReceive('getResponseHeaders')
+      ->once()
+      ->andReturn(explode("\n", trim($this->fakeRawHeader)));
+    $this->streamMock
+      ->shouldReceive('fileGetContents')
+      ->once()
+      ->with('http://foo.com/')
+      ->andReturn($this->fakeRawBody);
 
-        $this->streamClient->send('http://foo.com/', 'GET', 'foo_body', [], 60);
-    }
+    $this->streamClient->addRequestHeader('X-foo', 'bar');
+    $responseBody = $this->streamClient->send('http://foo.com/');
+
+    $this->assertEquals($responseBody, $this->fakeRawBody);
+    $this->assertEquals($this->streamClient->getResponseHeaders(), $this->fakeHeadersAsArray);
+    $this->assertEquals(200, $this->streamClient->getResponseHttpStatusCode());
+  }
+
+  /**
+   * @expectedException \Facebook\FacebookSDKException
+   */
+  public function testThrowsExceptionOnClientError()
+  {
+    $this->streamMock
+      ->shouldReceive('streamContextCreate')
+      ->once()
+      ->andReturn(null);
+    $this->streamMock
+      ->shouldReceive('getResponseHeaders')
+      ->once()
+      ->andReturn(null);
+    $this->streamMock
+      ->shouldReceive('fileGetContents')
+      ->once()
+      ->with('http://foo.com/')
+      ->andReturn(false);
+
+    $this->streamClient->send('http://foo.com/');
+  }
+
 }
