48467574f307050ca52c83209d09a182a49a1c4f
Alberto Anceschi <anceschialberto@icloud.com>
2015-11-11 23:27:21 +0100

    overwritten block--user--login template file

---
+++ docroot/sites/all/themes/subtheme/templates/block--user--login.tpl.php
@@ -0,0 +1,45 @@
+<?php
+$elements = drupal_get_form('user_login_block');
+$rendered = drupal_render($elements);
+
+$output = '
+<ul class="nav navbar-nav navbar-right">
+	<li class="leaf">
+    <a href="/user/register" title="Create a new user account.">Sign Up</a>
+  </li>
+	<li class="expanded dropdown">
+    <a href="#" title data-target="#" class="dropdown-toggle nolink" data-toggle="dropdown" >
+			Sign in
+			<span class="caret"></span>
+		</a>
+		<ul class="dropdown-menu" style="padding: 15px; min-width: 250px;">
+			<li class="first leaf">
+				<div class="row">
+					<div class="col-md-12">';
+
+$output  .= '<form action="' . $elements['#action'] . '" method="' . $elements['#method'] .
+            '" id="' . $elements['#id'] .
+            '" accept-charset="UTF-8"><div>';
+$output .= $elements['name']['#children'];
+$output .= $elements['pass']['#children'];
+$output .= $elements['form_build_id']['#children'];
+$output .= $elements['form_id']['#children'];
+$output .= $elements['actions']['#children'];
+$output .= '<div class="sign-in-links">' . $elements['links']['#children'] . '</div>';
+$output .= '</div></form>';
+print $output;
+
+$output .= '
+          </div>
+        </div>
+      </li>
+      <!-- <li class="divider">&nbsp;</li>
+      <li>
+      <input class="btn btn-primary btn-block" id="sign-in-google" type="button" value="Sign In with Google" />
+      <input class="btn btn-primary btn-block" id="sign-in-twitter" type="button" value="Sign In with Twitter" />
+      </li> -->
+    </ul>
+  </li>
+</ul>';
+
+?>
--- docroot/sites/all/themes/subtheme/templates/user-login.tpl.php
+++
@@ -1,4 +0,0 @@
-<p><?php print render($intro_text); ?></p>
-<div class="subtheme-user-login-form-wrapper-STICAZZI">
-  <?php print drupal_render_children($form) ?>
-</div>
