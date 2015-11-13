dce922d98941bbc2c84c3ea75e44ec1dcdba7de6
Alberto Anceschi <anceschialberto@icloud.com>
2015-11-13 17:40:23 +0100

    added analytics tracking

--- docroot/sites/all/themes/subtheme/template.php
+++ docroot/sites/all/themes/subtheme/template.php
@@ -4,5 +4,32 @@
  * @file
  * template.php
  */
- 
+ if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+   global $user;
+   global $tag;
+
+   print_r($tag);
+
+  //  // SETTING SOME COOKIES IN ORDER TO RECOGNISE THE user
+  //  if(isset($COOKIE['parseId']) && $COOKIE['parseId']!=null){
+  //    $cookie = $COOKIE['parseId'];
+  //    setcookie('parseId', $cookie, time()+3600*24*60); //per 2 mesi
+  //  } else {
+  //    $parseId = rand(9999999999999,1);
+  //    $cookie = $parseId;
+  //    setcookie('parseId', $parseId, time()+3600*24*60); //per 2 mesi
+  //  }
+
+
+   //Setting array parse with variables I will need in javascript
+   $parse = array(
+     //'parseId' => $cookie,
+     'uid' => $user->uid,
+     'tag' => $tag,
+     'debug' => true
+   );
+
+   drupal_add_js(array('parse'=>$parse), 'setting');
 ?>
--- docroot/sites/all/themes/subtheme/templates/html.tpl.php
+++ docroot/sites/all/themes/subtheme/templates/html.tpl.php
@@ -43,7 +43,9 @@
  *
  * @ingroup themeable
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <head profile="<?php print $grddl_profile; ?>">
@@ -57,6 +59,179 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
   <?php print $scripts; ?>
+
+  <!-- INIZIO PARTE DI PARSE -->
+  <script src="//www.parsecdn.com/js/parse-1.6.7.min.js"></script>
+
+  <script type="text/javascript">
+    Parse.initialize("P60EfTUuOZoeZyD2qSLpOrc8DWwUk2YjEqU2HY1R", "JlgM5RtBO3nYKd9YqvFho8Su9xppRmwcRxRpaeIV");
+
+//comandi parse
+    // Simple syntax to create a new subclass of Parse.Object.
+    var Visualizzazione = Parse.Object.extend("visualizzazione");
+
+    // Create a new instance of that class.
+    var visualizzazione = new Visualizzazione();
+    //visualizzazione.set({ parseId: Drupal.settings.parse.parseId.toString()});
+    visualizzazione.set({ user: Drupal.settings.parse.uid.toString()});
+    visualizzazione.set({ date: new Date().getTime()});
+    visualizzazione.set({ url: window.location.pathname});
+    visualizzazione.set({ tag: Drupal.settings.parse.tag})
+
+    visualizzazione.save(null, {
+      success: function(visualizzazione) {
+        // Execute any logic that should take place after the object is saved.
+        if(Drupal.settings.parse.debug){
+            console.log('New object created with objectId: ' + visualizzazione.id);
+        }
+      },
+      error: function(gameScore, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        if(Drupal.settings.parse.debug){
+            alert('Failed to create new object, with error code: ' + error.message);
+        }
+      }
+    });
+  </script>
+
+
+
+  <script type="text/javascript">
+//Script per vedere quanto hai letto di un articolo
+    jQuery(function($) {
+      // Debug flag
+      var debugMode = false;
+
+      // Default time delay before checking location
+      var callBackTime = 100;
+
+      // # px before tracking a reader
+      var readerLocation = 250;
+
+      // Set some flags for tracking & execution
+      var timer = 0;
+      var scroller = false;
+      var endContent = false;
+      var didComplete = false;
+
+      // Set some time variables to calculate reading time
+      var startTime = new Date();
+      var beginning = startTime.getTime();
+      var totalTime = 0;
+
+      // Get some information about the current page
+      var pageTitle = document.title;
+
+      // Track the article load
+      if (!debugMode) {
+          var Reading = Parse.Object.extend("Reading");
+
+          var reading = new Reading();
+          reading.set({pageLoaded: window.location.pathname});
+          reading.save(null, {
+            success:function(reading){
+              if(Drupal.settings.parse.debug){
+                console.log('New Reading created with objectId: ' + reading.id);
+              }
+            },
+            error: function(reading, error){
+              if(Drupal.settings.parse.debug){
+                alert('Failed to create new object, with error code: ' + error.message);
+              }
+            }
+          });
+      } else {
+          alert('The page has loaded. Woohoo.');
+      }
+
+      // Check the location and track user
+      function trackLocation() {
+          bottom = $(window).height() + $(window).scrollTop();
+          height = $(document).height();
+
+          // If user starts to scroll send an event
+          if (bottom > readerLocation && !scroller) {
+              currentTime = new Date();
+              scrollStart = currentTime.getTime();
+              timeToScroll = Math.round((scrollStart - beginning) / 1000);
+              if (!debugMode) {
+                reading.set({startReading: timeToScroll});
+                reading.save(null, {
+                  success:function(reading){
+                    if(Drupal.settings.parse.debug){
+                      console.log('Object reading ' + reading.id + " read in " + timeToScroll);
+                    }
+                  },
+                  error: function(reading, error){
+                    if(Drupal.settings.parse.debug){
+                      alert('Failed to create new object, with error code: ' + error.message);
+                    }
+                  }
+                });
+              } else {
+                  alert('started reading ' + timeToScroll);
+              }
+              scroller = true;
+          }
+
+          // If user has hit the bottom of the content send an event (la segnalazione parte un po' prima ad essere sinceri)
+          if (bottom >= $('#block-system-main').scrollTop() + $('#block-system-main').innerHeight() && !endContent) {
+              currentTime = new Date();
+              contentScrollEnd = currentTime.getTime();
+              timeToContentEnd = Math.round((contentScrollEnd - scrollStart) / 1000);
+              if (!debugMode) {
+                  reading.set({pageBottom: timeToContentEnd});
+                  reading.save(null, {
+                    success:function(reading){
+                      if(Drupal.settings.parse.debug){
+                        console.log('Object bottom ' + reading.id + " read in " + timeToContentEnd);
+                      }
+                    },
+                    error: function(reading, error){
+                      if(Drupal.settings.parse.debug){
+                        alert('Failed to create new object, with error code: ' + error.message);
+                      }
+                    }
+                  });
+              } else {
+                  alert('end content section '+timeToContentEnd);
+              }
+              endContent = true;
+          }
+
+          // If user has hit the bottom of page send an event
+          if (bottom >= height && !didComplete) {
+              currentTime = new Date();
+              end = currentTime.getTime();
+              totalTime = Math.round((end - scrollStart) / 1000);
+              if (!debugMode) {
+                  if (totalTime < 60) {
+                      _gaq.push(['_setCustomVar', 5, 'ReaderType', 'Scanner', 2]);
+                  } else {
+                      _gaq.push(['_setCustomVar', 5, 'ReaderType', 'Reader', 2]);
+                  }
+                  _gaq.push(['_trackEvent', 'Reading', 'PageBottom', pageTitle, totalTime]);
+              } else {
+                  alert('bottom of page '+totalTime);
+              }
+              didComplete = true;
+          }
+      }
+
+      // Track the scrolling and track location
+      $(window).scroll(function() {
+          if (timer) {
+              clearTimeout(timer);
+          }
+
+          // Use a buffer so we don't call trackLocation too often.
+          timer = setTimeout(trackLocation, callBackTime);
+      });
+  });
+  </script>
+  <!-- FINE PARTE DI PARSE -->
+
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
